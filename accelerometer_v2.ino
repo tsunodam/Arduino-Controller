b2aba5eb0ac3a697e8e359f4eff5a039908637b2
tsunodam <60412897+tsunodam@users.noreply.github.com>
2020-03-02 23:51:41 -0500

    Update accelerometer_v2.ino

--- accelerometer_v2.ino
+++ accelerometer_v2.ino
@@ -51,36 +51,27 @@ void loop() {
 
   // PYTHON CONNECTION: Prints a letter to serial monitor to press down
   // appropriate key.
-  
   if (zScaled < -7000) {     // Jump
     Serial.println('U');
     while (zScaled < -7000 && count < 12) {
       count ++;
       zRaw = analogRead(zInput);
       zScaled = map(zRaw, zRawMax, zRawMin, -3000, 3000);
-//      Serial.print("Loop 4  Count: ");
-//      Serial.println(count);
     }
     count = 0;
     while (zScaled >= -7000 && count < 40) {
       count ++;
       zRaw = analogRead(zInput);
       zScaled = map(zRaw, zRawMax, zRawMin, -3000, 3000);
-//      Serial.print("Loop 5  Count: ");
-//      Serial.println(count);
     }
     count = 0;
     while (zScaled < -7000 && count < 12) {
       count ++;
       zRaw = analogRead(zInput);
       zScaled = map(zRaw, zRawMax, zRawMin, -3000, 3000);
-//      Serial.print("Loop 6  Count: ");
-//      Serial.println(count);
     }
     count = 0;
-//    Serial.println("JUMPED");
     delay(800); //750
-//    Serial.println("JUMPED");
   }
 
   if (zScaled > -20) {       // Squat
@@ -89,154 +80,72 @@ void loop() {
       count ++;
       zRaw = analogRead(zInput);
       zScaled = map(zRaw, zRawMax, zRawMin, -3000, 3000);
-//      Serial.print("Loop 1  Count: ");
-//      Serial.println(count);
     }
     count = 0;
     while (zScaled <= -20 && count < 12) {
       count ++;
       zRaw = analogRead(zInput);
       zScaled = map(zRaw, zRawMax, zRawMin, -3000, 3000);
-//      Serial.print("Loop 2  Count: ");
-//      Serial.println(count);
     }
     count = 0;
     while (zScaled > -20 && count < 12) {
       count ++;
       zRaw = analogRead(zInput);
       zScaled = map(zRaw, zRawMax, zRawMin, -3000, 3000);
-//      Serial.print("Loop 3  Count: ");
-//      Serial.println(count);
     }
-//    Serial.println("SQUATTED");
     count = 0;
     delay(700);
-//    Serial.println("SQUATTED");
   }
   
-  if (xScaled > 1300) {
-//    Serial.println(xScaled);
+  if (xScaled > 1300) {       // Left
     Serial.println('L');
     count = 0;
     while (xScaled > 1300 && count < 30) {
       count ++;
       xRaw = analogRead(xInput);
       xScaled = map(xRaw, xRawMax, xRawMin, -3000, 3000);
-//      Serial.print("Loop D  Count: ");
-//      Serial.println(count);
     }
-//    Serial.println("HERE");
     count = 0;
     while (xScaled <= 1300 && xScaled >= -1300 && count < 30) {
       count ++;
       xRaw = analogRead(xInput);
       xScaled = map(xRaw, xRawMax, xRawMin, -3000, 3000);
-//      Serial.print("Loop E  Count: ");
-//      Serial.println(count);
     }
     count = 0;
     while (xScaled < -1300 && count < 30) {
       count ++;
       xRaw = analogRead(xInput);
       xScaled = map(xRaw, xRawMax, xRawMin, -3000, 3000);
-//      Serial.print("Loop F  Count: ");
-//      Serial.println(count);
     }
     count = 0;
     delay(400);
-//    Serial.println("LEFT");
   }
-
-
-
-  if (xScaled < -1300) {
+  
+  if (xScaled < -1300) {        // Right
     Serial.println('R');
     while (xScaled < -1300 && count < 30) {
       count ++;
       xRaw = analogRead(xInput);
       xScaled = map(xRaw, xRawMax, xRawMin, -3000, 3000);
-//      Serial.print("Loop A  Count: ");
-//      Serial.println(count);
     }
     count = 0;
     while (xScaled >= -1500 && xScaled <= 1500 && count < 30) {
       count ++;
       xRaw = analogRead(xInput);
       xScaled = map(xRaw, xRawMax, xRawMin, -3000, 3000);
-//      Serial.print("Loop B  Count: ");
-//      Serial.println(count);
     }
     count = 0;
     while (xScaled > 1500 && count < 30) {
       count ++;
       xRaw = analogRead(xInput);
       xScaled = map(xRaw, xRawMax, xRawMin, -3000, 3000);
-//      Serial.print("Loop C  Count: ");
-//      Serial.println(count);
     }
     count = 0;
     delay(400);
-//    Serial.println("RIGHT");
   }
 
-  
-
-
-//  if (xScaled < -1300) {      // Right-Step -1300, 1300, 1000
-//    while (xScaled < 1300 && count < 30) {
-//      count ++;
-//      xRaw = analogRead(xInput);
-//      xScaled = map(xRaw, xRawMax, xRawMin, -3000, 3000);
-////      Serial.print("Loop 7  Count: ");
-////      Serial.println(count);
-//    }
-//    Serial.println('R');
-//    count = 0;
-//    if (xScaled >= 1300) {
-//      Serial.println('R');
-//      xRaw = analogRead(xInput);
-//      xScaled = map(xRaw, xRawMax, xRawMin, -3000, 3000);
-//    }
-//    while(xScaled > 1000 && count < 10) {
-//      count ++;
-//      xRaw = analogRead(xInput);
-//      xScaled = map(xRaw, xRawMax, xRawMin, -3000, 3000);
-////      Serial.print("Loop 8  Count: ");
-////      Serial.println(count);
-//    }
-//    count = 0;
-//    xRaw = analogRead(xInput);
-//    xScaled = map(xRaw, xRawMax, xRawMin, -3000, 3000);\
-//    delay(400);
-////    Serial.println("RIGHT");
-//  }
-
-  if (xScaled > 1300) {       // Left-Step 1300, -1300, -1000
-    while (xScaled > -1300 && count < 40) {
-      count ++;
-      xRaw = analogRead(xInput);
-      xScaled = map(xRaw, xRawMax, xRawMin, -3000, 3000);
-//      Serial.print("Loop 9  Count: ");
-//      Serial.println(count);
-    }
-//    Serial.println('L');
-//    count = 0;
-//    if (xScaled <= -1300) {
-////      Serial.println('L');
-//      xRaw = analogRead(xInput);
-//      xScaled = map(xRaw, xRawMax, xRawMin, -3000, 3000);
-//    }
-//    while (xScaled < -1000 && count < 15) {
-//      count ++;
-//      xRaw = analogRead(xInput);
-//      xScaled = map(xRaw, xRawMax, xRawMin, -3000, 3000);
-////      Serial.print("Loop 10  Count: ");
-////      Serial.println(count);
-//    }
-//    count = 0;
-//    xRaw = analogRead(xInput);
-//    xScaled = map(xRaw, xRawMax, xRawMin, -3000, 3000);
-//    delay(400);
-////    Serial.println("LEFT");
-  }
 }
+
+
+
+
